fcd3a73a883391c80635ec2d9aa8c71472bfc353
Harrison Powers <fijimunkii@gmail.com>
2015-10-19 20:58:04 -0400

    Refactor messaging

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
   Description:
     Hubot uptime monitor / pingdom script
 
+  Configuration:
+
   Commands:
     hubot add ping url
     hubot remove ping url
@@ -14,20 +16,34 @@ var request = require('request');
 
 module.exports = function(hubot) {
 
-  setInterval(pingdome.bind(null, hubot), interval);
+  function sendMessage(message, msg) {
+    console.log(msg);
+    hubot.messageRoom(room, msg);
+    if (message && message.envelope.room !== room) message.send(msg);
+  }
+
+  function pingdome() {
+    (hubot.brain.get('pings') || []).forEach(function(url) {
+      request(url, function(err, res, body) {
+        if ([200,403,"200","403"].indexOf(res.statusCode) < 0) {
+          sendMessage(null, url + ' is DOWN!');
+        }
+      });
+    });
+  }
+
+  setInterval(pingdome, interval);
 
   hubot.respond(/add ping (\S+)/i, function(message) {
     var url = message.match[1];
     var pings = hubot.brain.get('pings') || [];
     if (pings.filter(function(d) { return d === url; }).length) {
-      console.log('ping already added: ' + url);
-      hubot.messageRoom('ping already added: ' + url);
+      sendMessage(message, 'ping already added: ' + url);
     } else {
       pings.push(url);
       hubot.brain.set('pings', pings);
       hubot.brain.save();
-      console.log('added ping: ' + url);
-      hubot.messageRoom(room, 'added ping: ' + url);
+      sendMessage(message, 'added ping: ' + url);
     }
   });
   hubot.respond(/remove ping (\S+)/i, function(message) {
@@ -38,27 +54,13 @@ module.exports = function(hubot) {
     if (pings.length < len) {
       hubot.brain.set('pings', pings);
       hubot.brain.save();
-      console.log('removed ping: ' + url);
-      hubot.messageRoom(room, 'removed ping: ' + url);
+      sendMessage(message, 'removed ping: ' + url);
     } else {
-      console.log('ping not found or removed: ' + url);
-      hubot.messageRoom('ping not found or removed: ' + url);
+      sendMessage(message, 'ping not found or removed: ' + url);
     }
   });
-  hubot.respond(/list ping(s?)/i, function(message) {
-    console.log(JSON.stringify(hubot.brain.get('pings') || []));
-    hubot.messageRoom(JSON.stringify(pings));
+  hubot.respond(/list pings/i, function(message) {
+    sendMessage(message, (hubot.brain.get('pings') || []).join(' '));
   });
 
 };
-
-function pingdome(hubot) {
-  (hubot.brain.get('pings') || []).forEach(function(url) {
-    request(url, function(err, res, body) {
-      if ([200,403,"200","403"].indexOf(res.statusCode) < 0) {
-        console.log(url + ' is DOWN!');
-        hubot.messageRoom(room, url + ' is DOWN!');
-      }
-    });
-  });
-}
